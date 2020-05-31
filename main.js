38712fbe1efc9db4b59c5acc3ea6ba54ca7547ad
edwinwalela <edwinwalela@gmail.com>
2020-05-31 11:59:41 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    function getLocation(){
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(getWeather);
+        }else{
+            alert("Geolocation not supported by this browser");
+        }
+    }
+
+    function getWeather(position){
+        let lat = position.coords.latitude;
+        let long = position.coords.longitude;
+        let API_KEY = '9d646800231533d6e07ae7d77f155987';
+        let baseURL = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${API_KEY}`;
+
+        $.get(baseURL,function(res){
+            let data = res.current;
+            let temp = Math.floor(data.temp - 273);
+            let condition = data.weather[0].description;
+
+            $('#temp-main').html(`${temp}°`);
+            $('#condition').html(condition);
+        })
+        
+    }
+
+    getLocation();
+})
